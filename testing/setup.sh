7b941ff2e18266e4f9a527ff855241c466920eab
Lenard Frommelt <lenny09918050@gmail.com>
2026-09-05 21:55:03 +0300

    thingysync push

--- testing/setup.sh
+++ testing/setup.sh
@@ -5,7 +5,9 @@
 systemctl stop sentinel-access-manager.socket
 systemctl stop sentinel-access-manager.service
 systemctl stop sentinel-access-manager.path
+
 rm /run/sentinel-access-manager.sk
+
 #endregion
 
 ############################################################
@@ -14,15 +16,20 @@ cp sentinel-access-manager.service /etc/systemd/system/
 cp sentinel-access-manager.socket /etc/systemd/system/
 cp sentinel-access-manager.path /etc/systemd/system/
 cp restart-sentinel-access-manager.service /etc/systemd/system/
+
 cp nginx-config /etc/nginx/servers/sentinel-access-manager
 
-cp ../output/service.js /opt/void/sentinel-access-manager/service.js
 #endregion
+
+############################################################
+./mount-files.sh
 
 ############################################################
 #region reloadAnd(Re)start
 systemctl daemon-reload
 systemctl start sentinel-access-manager.socket
 systemctl start sentinel-access-manager.path
+
 nginx -s reload
+
 #endregion
